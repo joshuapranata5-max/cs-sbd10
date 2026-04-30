a7a3580a7573b4dcfdd4db11cc2edd6936697d8c
ValiantJoshua <valiant.joshua5@gmail.com>
2026-04-30 23:59:54 +0700

    Update redis connection to support Upstash REDIS_URL

--- backend/src/database/redis.js
+++ backend/src/database/redis.js
@@ -1,11 +1,11 @@
 const Redis = require("ioredis");
 
-const redis = new Redis();
-
-new Redis({
-  port: 6379, // Redis port
-  host: "127.0.0.1", // Redis host
-});
+const redis = process.env.REDIS_URL 
+  ? new Redis(process.env.REDIS_URL) 
+  : new Redis({
+      port: 6379,
+      host: "127.0.0.1",
+    });
 
 redis.on("connect", () =>{
     console.log("Connected to redis's server");
