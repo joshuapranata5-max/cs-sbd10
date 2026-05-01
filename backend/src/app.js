a2755f3ce8af4dec581d4ae794545149760f1458
ValiantJoshua <valiant.joshua5@gmail.com>
2026-05-01 15:05:43 +0700

    Fix Vercel routing and CORS issues

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  origin: true, // Allow all origins securely
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   credentials: true
 }));
 
